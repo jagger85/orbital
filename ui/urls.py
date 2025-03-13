50c1f36ddbcd66c7feb02760e62abde5f9d16ee0
Jagger85 <andresfonsortega@yahoo.es>
2025-03-13 17:30:40 +0700

    Feat(ui/urls&views): - Configured urls - Implemented htmx view middleware and logic to handle the navigation requests

--- ui/urls.py
+++ ui/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index ),
+    path('', views.overview ),
+    path('scrap/', views.scrapers ),
+    path('automation/', views.automation ),
+    path('history/', views.history ),
+    path('user-management/', views.user_management ),
+    path('preferences/', views.settings ),
 ]
--- ui/views.py
+++ ui/views.py
@@ -1,4 +1,45 @@
 from django.shortcuts import render
 
-def index(request):
-    return render(request,'ui/index.html')
+def htmx_view(request, template_base):
+    """
+    Generic view handler for HTMX requests
+    
+    This handler is needed to correctly send the views when 
+    the user reloads the page or when is triggered by an htmx element
+    
+    - When the user refreshes the browser we need to send the whole page 
+
+    - When is triggered by a HTMX navigation link we only need to send the partial
+
+    """
+
+    context = {
+        'partial_template': f"ui/partials/{template_base}.html"
+    }
+    template = f"ui/partials/{template_base}.html" if request.htmx else "ui/main-layout.html"
+
+    return render(request, template, context)
+
+
+def overview(request):
+    return htmx_view(request, 'overview')
+
+
+def scrapers(request):
+    return htmx_view(request, 'scrapers')
+
+
+def automation(request):
+    return htmx_view(request, 'automation')
+
+
+def history(request):
+    return htmx_view(request, 'history')
+
+
+def user_management(request):
+    return htmx_view(request, 'user-management')
+
+
+def settings(request):
+    return htmx_view(request, 'settings')
