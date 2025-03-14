2d97b2819cd9d6afc6a7de38ec6dbba5c1162f5f
Jagger85 <andresfonsortega@yahoo.es>
2025-03-14 20:02:39 +0700

    Feat(ui/dialog-views-urls): Implemented dialog logic and registered urls

--- ui/views.py
+++ ui/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+# Page views and htmx middleware 
+
 def htmx_view(request, template_base):
     """
     Generic view handler for HTMX requests
@@ -20,26 +22,50 @@ def htmx_view(request, template_base):
 
     return render(request, template, context)
 
-
 def overview(request):
     return htmx_view(request, 'overview')
-
 
 def scrapers(request):
     return htmx_view(request, 'scrapers')
 
-
 def automation(request):
     return htmx_view(request, 'automation')
-
 
 def history(request):
     return htmx_view(request, 'history')
 
-
 def user_management(request):
     return htmx_view(request, 'user-management')
 
-
 def settings(request):
     return htmx_view(request, 'settings')
+
+# Dialog Forms
+
+def dialog_form_view(request, context):
+    """
+    Generic view handler for Dialog requests
+    
+    This handler is intended to reuse base-form-dialog.html
+
+    It injects the desired form partial
+
+    """
+
+    return render(request, 'ui/components/dialogs/base-form-dialog.html', context)
+
+def add_user(request):
+    context = {
+        'dialog_title': 'Add User',
+        'request_path': '///////',
+        'form_template': 'ui/components/dialogs/add-user-form.html',
+    }
+    return dialog_form_view(request, context)
+
+def add_scrap(request):
+    context ={
+        'dialog_title': 'Add scrap',
+        'request_path': '///////',
+        'form_template': 'ui/components/dialogs/add-scrap-form.html'
+    }
+    return dialog_form_view(request, context)
