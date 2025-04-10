2d489e1480dad542c09dad443695e17ba52ca4b4
Jagger85 <andresfonsortega@yahoo.es>
2025-04-10 19:39:24 +0200

    Feat(ui/icons): Added download, trash and file icons

--- ui/templatetags/icons.py
+++ ui/templatetags/icons.py
@@ -48,4 +48,20 @@ ICONS = {
         <circle cx="12" cy="12" r="10"/>
         <path d="m9 12 2 2 4-4"/>
     """),
+    "download": get_icon("download", """"
+        <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/>
+        <polyline points="7 10 12 15 17 10"/><line x1="12" x2="12" y1="15" y2="3"/>
+    """),
+    "trash": get_icon("trash", """"
+        <path d="M3 6h18"/><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/>
+        <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/><line x1="10" x2="10" y1="11" y2="17"/>
+        <line x1="14" x2="14" y1="11" y2="17"/>              
+    """),
+     "file": get_icon("file", """"
+        <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z"/>
+        <path d="M14 2v4a2 2 0 0 0 2 2h4"/>
+        <path d="M10 9H8"/>
+        <path d="M16 13H8"/>
+        <path d="M16 17H8"/>              
+    """),
 }
